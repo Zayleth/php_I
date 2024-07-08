a8b77bac806deaf6f6ccc49db635676e65302ef2
Zayleth <zaylethvegas@gmail.com>
2024-07-08 16:00:51 -0400

    Product Database - Search, tables

--- buscador.php
+++ buscador.php
@@ -3,23 +3,21 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Show Products</title>
+  <title>Search Products</title>
 </head>
 <body>
-  <h1>Inventory - Product data</h1>
+  <h1>Search - Products data</h1>
+
+  <form action="" method="post">
+    <input type="text" name="search"><button type="submit">Search product name</button>
+  </form>
+
+  <br><br>
+
   <table border="1">
     <tr>
       <th>ID</th>
-      <th>Name
-
-        <a href="mostrar_producto.php?n=nombre_producto&orden_alfabetico=asc">asc
-        </a>
-
-        <a href="mostrar_producto.php?n=nombre_producto&orden_alfabetico=desc">desc
-        </a>
-
-      </th>
-
+      <th>Name</th>
       <th>Price</th>
       <th>Amount</th>
       <th>Due date</th>
@@ -30,30 +28,17 @@
     <?php
     include "conexion.php";
 
-    // para ordenar el nombre_producto en orden alfabetico
-    if(isset ($_GET['n'])) {
-      $sql = "SELECT * FROM productos ORDER BY ".$_GET['n']." ".$_GET['orden_alfabetico'];
+    // buscador de productos: 
+
+    if(isset($_POST['search'])) {
+      $sql = "SELECT * FROM productos where nombre_producto like '$_POST[search]%'";
     
     } else {
       $sql = "SELECT * FROM productos";
     }
+
 
     $guardar_consuta = mysqli_query($conex, $sql);
-
-
-    /* BUSCAR como usar para darle click y que se ordene de la a - z y viceversa
-
-    if(@$_GET['n'] == 1) {
-      $orden = "asc";
-      $ordenar_a_z = 2;
-    
-    } else if ($ordenar_a_z == 2) {
-      $orden = "desc";
-    
-    } else {
-      $sql = "SELECT * FROM productos";
-    }
-    */
 
 
     // mysqli_fetch_array($guardar_consuta) -> agarrame estos valores de un arreglo -> posiciones
