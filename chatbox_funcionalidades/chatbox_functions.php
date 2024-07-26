d325267e2fa48be8d92c9bc2ea9b81d28d2d0318
Zayleth <zaylethvegas@gmail.com>
2024-07-26 16:49:45 -0400

    CHATBOT Creation - Functionalities

---
+++ chatbox_funcionalidades/chatbox_functions.php
@@ -0,0 +1,26 @@
+<?php
+function chat() {
+?>
+
+
+<div id="chatbox">
+<div id="cabecera">
+<div id="imagen"></div>
+<div id="nombre"></div>
+</div>
+
+<div id="hablar">
+<form action="respuesta_chatbot.php" method="post">
+  <input type="text" name="pregunta">
+  <button type="submit">ASK</button>
+</form>
+</div>
+
+</div>
+
+
+
+
+<?php
+}
+?>
